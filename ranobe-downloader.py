3d8fd8766dde1f65da59b31c4426c05cc77bbb34
kemu <167982813+kemuvx@users.noreply.github.com>
2024-10-27 21:23:12 +0700

    initial

---
+++ ranobe-downloader.py
@@ -0,0 +1,231 @@
+import os
+import time
+import mkepub
+import shutil
+from utils import get_ranobe_info, get_volume_chapters, download_cover, remove_bad_chars, get_chapter_content
+RANOBE_NAME = input("Название ранобе: ")
+RANOBE_VOLUME = input("Том: ").strip()
+
+STYLE = """
+@page {
+    margin-bottom: 5pt;
+    margin-top: 5pt
+    }
+
+.block_ {
+    display: block;
+    font-size: 0.83333em;
+    line-height: 1.2;
+    text-align: center;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_1 {
+    display: block;
+    font-family: serif;
+    font-size: 0.83333em;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_2 {
+    display: block;
+    font-size: 0.83333em;
+    line-height: 1.2;
+    text-align: center;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_3 {
+    display: block;
+    font-size: 1.29167em;
+    font-weight: bold;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_4 {
+    display: block;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_5 {
+    display: block;
+    font-size: 0.83333em;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_6 {
+    display: block;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_7 {
+    display: block;
+    font-size: 1.29167em;
+    font-weight: bold;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_8 {
+    display: block;
+    font-size: 1em;
+    font-weight: bold;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_9 {
+    display: block;
+    font-size: 1em;
+    font-weight: bold;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_10 {
+    display: block;
+    font-weight: bold;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.block_11 {
+    display: block;
+    font-size: 1.29167em;
+    font-weight: normal;
+    line-height: 1.2;
+    margin: 0 0 10pt;
+    padding: 0
+    }
+.calibre {
+    color: black;
+    display: block;
+    font-family: "Arial", sans-serif;
+    font-size: 1em;
+    padding-left: 0;
+    padding-right: 0;
+    margin: 0 5pt
+    }
+.calibre1 {
+    display: block;
+    height: auto;
+    line-height: 1.2;
+    width: auto
+    }
+.calibre2 {
+    font-size: 1em;
+    line-height: 1.2;
+    vertical-align: super
+    }
+.calibre3 {
+    font-size: 0.75em;
+    line-height: 1.2;
+    vertical-align: super
+    }
+.calibre4 {
+    line-height: 1.2
+    }
+.calibre5 {
+    display: block;
+    font-size: 1.29167em;
+    line-height: 1.2
+    }
+.calibre6 {
+    line-height: 1.2;
+    text-decoration: none
+    }
+.calibre7 {
+    display: block;
+    margin-left: 40px
+    }
+.footnote {
+    display: block;
+    margin: 1em 0
+    }
+.footnote1 {
+    display: block;
+    page-break-after: avoid;
+    margin: 1em 0
+    }
+.noteref {
+    text-decoration: none
+    }
+.text_ {
+    font-size: 1.2em;
+    line-height: 1.2
+    }
+
+"""
+URL_TO_CHAPTERS = "https://api.lib.social/api/manga/" + RANOBE_NAME + "/chapters"
+URL_TO_RANOBE = "https://api.lib.social/api/manga/" + RANOBE_NAME + ("?fields[]=background&fields[]=eng_name&fields["
+                                                                     "]=otherNames&fields[]=summary&fields["
+                                                                     "]=releaseDate&fields[]=type_id&fields["
+                                                                     "]=caution&fields[]=views&fields["
+                                                                     "]=close_view&fields[]=rate_avg&fields["
+                                                                     "]=rate&fields[]=genres&fields[]=tags&fields["
+                                                                     "]=teams&fields[]=franchise&fields["
+                                                                     "]=authors&fields[]=publisher&fields["
+                                                                     "]=userRating&fields[]=moderated&fields["
+                                                                     "]=metadata&fields[]=metadata.count&fields["
+                                                                     "]=metadata.close_comments&fields["
+                                                                     "]=manga_status_id&fields[]=chap_count&fields["
+                                                                     "]=status_id&fields[]=artists&fields[]=format")
+print(f"url к информации о ранобе: {URL_TO_RANOBE}\n")
+print(f"url к главам: {URL_TO_CHAPTERS}\n")
+
+try:
+    shutil.rmtree("cover")
+except:
+    pass
+try:
+    shutil.rmtree("images")
+except:
+    pass
+os.makedirs("cover", exist_ok=True)
+os.makedirs("images", exist_ok=True)
+ranobe_info_dict = get_ranobe_info(URL_TO_RANOBE)
+ranobe_title = ranobe_info_dict["title"]
+ranobe_description = ranobe_info_dict["description"]
+ranobe_cover = ranobe_info_dict["cover_url"]
+ranobe_author = ranobe_info_dict["author"]
+ranobe_chapters_dict = get_volume_chapters(url=URL_TO_CHAPTERS, volume=RANOBE_VOLUME)
+
+download_cover(ranobe_cover)
+
+book = mkepub.Book(title=ranobe_title, author=ranobe_author, description=ranobe_description)
+with open('cover/cover.jpg', 'rb') as file:
+    book.set_cover(file.read())
+book.set_stylesheet(STYLE)
+
+book.add_page(title=f"{ranobe_title} Том {RANOBE_VOLUME}", content=f"""
+<h1>{ranobe_title} Том  {RANOBE_VOLUME}</h1>
+<p><i>
+{ranobe_description}
+</i></p>
+""")
+
+for chapter_num, chapter_name in ranobe_chapters_dict.items():
+    time.sleep(1)
+    url_to_chapter = ("https://api.lib.social/api/manga/" + RANOBE_NAME +
+                      "/chapter?number=" + str(chapter_num) + "&volume=" + str(RANOBE_VOLUME))
+
+    chapter_content, images_dict = get_chapter_content(url=url_to_chapter, chapter_num=chapter_num, chapter_name=chapter_name)
+    book.add_page(title=f"Глава {chapter_num}. {chapter_name}", content=chapter_content)
+    if images_dict:
+        for image in images_dict.values():
+            with open(image, 'rb') as image_file:
+                book.add_image(image[7:], image_file.read())
+
+
+book_name = remove_bad_chars(ranobe_title) + f" Том {RANOBE_VOLUME}.epub"
+try:
+    os.remove(book_name)
+except:
+    pass
+book.save(book_name)
+print(f'Книга сохранена как {book_name}')
---
+++ utils.py
@@ -0,0 +1,143 @@
+import requests
+import time
+import os
+from bs4 import BeautifulSoup
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+def get_ranobe_info(url:str) -> dict:
+    """Парсит cover_url, author, title, description"""
+    response = requests.get(url, headers=headers)
+    if response.status_code not in [200, 204]:
+        print(f"Ответ {response.status_code}: {url}")
+        time.sleep(5)
+        get_ranobe_info(url)
+    data = response.json()['data']
+    ranobe_info_dict = dict()
+    ranobe_info_dict['cover_url'] = data['cover']['default']
+    ranobe_info_dict['author'] = data['authors'][0]['rus_name'] if data['authors'][0]['rus_name'] is not None else data['authors'][0]['name']
+    try:
+        ranobe_info_dict['title'] = data['rus_name']
+    except:
+        ranobe_info_dict['title'] = data['name']
+    try:
+        ranobe_info_dict['description'] = data['summary']
+    except Exception as e:
+        ranobe_info_dict['description'] = ""
+    return ranobe_info_dict
+
+
+def get_volume_chapters(url:str, volume:str) -> dict:
+    """Парсит номер и название глав"""
+    response = requests.get(url, headers=headers)
+    data = response.json()['data']
+    chapters_dict = {}
+    for chapter in data:
+        if chapter['volume'] == volume:
+            chapter_num = chapter['number']
+            chapter_name = chapter['name'] if chapter['name'].strip() != "" else f"Глава {chapters_dict}"
+            chapters_dict[chapter_num] = chapter_name
+    return chapters_dict
+
+def download_cover(url:str) -> None:
+    cover_data = requests.get(url, headers=headers).content
+    with open('cover/cover.jpg', 'wb') as handler:
+        handler.write(cover_data)
+
+def remove_bad_chars(text:str) -> str:
+    bad_chars = '"?<>|\/:–'
+    for c in bad_chars:
+        text = text.replace(c, '')
+    return text
+
+
+def get_chapter_content(url:str, chapter_num:str, chapter_name:str) -> tuple[str, dict]:
+    response = requests.get(url, headers=headers)
+    json_response = response.json()
+    try:
+        json_response['data']['content']['type'] == "doc" # если ошибка значит легаси
+        is_legacy = False
+        print(f"\nГлава {chapter_num}: {chapter_name}: {url}  \nТип главы: не легаси")
+    except TypeError:
+        is_legacy = True
+        print(f"\nГлава {chapter_num}: {chapter_name}: {url}  \nТип главы: легаси")
+    os.makedirs("images", exist_ok=True)
+    images_dict = {}
+    image_counter = 1
+    if is_legacy:
+        content_soup = BeautifulSoup(json_response['data']['content'], 'lxml')
+        bad_sites = ["novel.tl", "ruranobe.ru", "rulate.ru"]
+
+        for img in content_soup.find_all('img'):
+            img_url = img['src']
+            if not any(x in img_url for x in bad_sites):
+                img_url = "https://ranobelib.me" + img_url
+            if img_url.count("ranobelib.me") > 1:
+                img_url = img_url[20:]
+            print(f"Арт {chapter_num}-{image_counter}: " + img_url)
+            with open(f'images/{chapter_num}-{image_counter}.jpg', 'wb') as f:
+                f.write(requests.get(img_url, headers=headers).content)
+            del img['loading']
+            img['src'] = f'images/{chapter_num}-{image_counter}.jpg'
+            images_dict[str(image_counter)] = img['src']
+            image_counter += 1
+        for p in content_soup.find_all('p'):
+            del p['data-paragraph-index']
+        content = ((f"<h1>Глава {str(chapter_num)}. {str(chapter_name)}</h1>" + str(content_soup))
+                   .replace('<html>', "")
+                   .replace("</html>", "")
+                   .replace("<body>", "")
+                   .replace("</body>", ""))
+
+
+    else:  # не легаси
+        attachments = json_response['data']['attachments']
+        chapter_content = json_response['data']['content']['content']
+        content = f"<h1>Глава {chapter_num}. {chapter_name}</h1>\n"
+        for element in chapter_content:
+            if element['type'] == 'image':
+                images = element['attrs']['images']
+                for image in images:
+                    image_id = image['image']
+                    for attachment in attachments:
+                        if attachment['name'] == image_id:
+                            img_url = ' https://ranobelib.me' + attachment['url']
+                            print(f"Арт {chapter_num}-{image_counter}: " + img_url)
+                            img_content = requests.get(img_url, headers=headers).content
+                            with open(f'images/{chapter_num}-{image_counter}.png', 'wb') as handler:
+                                handler.write(img_content)
+                            content += f'<p><img src="images/{chapter_num}-{image_counter}.png"></img></p>\n'
+                            images_dict[str(image_counter)] = f"images/{chapter_num}-{image_counter}.png"
+                            image_counter += 1
+            elif element['type'] == "paragraph":
+                if "content" in element:
+                    for line_of_element in element['content']:
+                        if line_of_element['type'] == 'text':
+                            if "marks" and "italic" in str(line_of_element):
+                                content += f"<p><i>{line_of_element['text']}</i></p>\n"
+                            else:
+                                content += f"<p>{line_of_element['text']}</p>\n"
+                        elif line_of_element['type'] == 'hardBreak':
+                            content += "<br>\n"
+                        else:
+                            print(line_of_element['text'])
+                            raise Exception(f"не текст {line_of_element['type']}, {line_of_element}")
+        
+                    content += f"<p></p>\n"
+            elif element['type'] == "heading":
+                if "content" in element:
+                    for line_of_element in element['content']:
+                        if line_of_element['type'] == 'text':
+                            content += f"<h3>{line_of_element['text']}</h3>\n"
+                        else:
+                            raise Exception(f"не текст {element}, {line_of_element}")
+            elif element['type'] == "horizontalRule" and "content" not in element:
+                pass
+            else:
+                raise Exception(f"Новый тип {element['type']}, {element}")
+    return content, images_dict
