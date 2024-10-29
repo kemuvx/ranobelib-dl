cf79328f31b2f86da741f74eabbfeea820265466
kemu <167982813+kemuvx@users.noreply.github.com>
2024-10-29 15:29:12 +0700

    фикс mkepub, парса глав без имени

--- ranobe-downloader.py
+++ ranobe-downloader.py
@@ -1,11 +1,13 @@
 import os
 import time
-import mkepub
+
 import shutil
-from utils import get_ranobe_info, get_volume_chapters, download_cover, remove_bad_chars, get_chapter_content, get_ranobe_name_from_url
-RANOBE_NAME = get_ranobe_name_from_url(input("Ссылка на ранобе: "))
-RANOBE_VOLUME = input("Том: ").strip()
-print(f"Ранобе {RANOBE_NAME}, том {RANOBE_VOLUME}")
+from utils import get_ranobe_info, get_volume_chapters, download_cover, remove_bad_chars, get_chapter_content, get_ranobe_name_from_url, Book
+
+
+ranobe_name = get_ranobe_name_from_url(input("Ссылка на ранобе: "))
+ranobe_volume = input("Том: ").strip()
+print(f"Ранобе {ranobe_name}, том {ranobe_volume}")
 STYLE = """
 @page {
     margin-bottom: 5pt;
@@ -161,8 +163,8 @@ STYLE = """
     }
 
 """
-URL_TO_CHAPTERS = "https://api.lib.social/api/manga/" + RANOBE_NAME + "/chapters"
-URL_TO_RANOBE = "https://api.lib.social/api/manga/" + RANOBE_NAME + ("?fields[]=background&fields[]=eng_name&fields["
+url_to_chapters = "https://api.lib.social/api/manga/" + ranobe_name + "/chapters"
+url_to_ranobe = "https://api.lib.social/api/manga/" + ranobe_name + ("?fields[]=background&fields[]=eng_name&fields["
                                                                      "]=otherNames&fields[]=summary&fields["
                                                                      "]=releaseDate&fields[]=type_id&fields["
                                                                      "]=caution&fields[]=views&fields["
@@ -175,8 +177,8 @@ URL_TO_RANOBE = "https://api.lib.social/api/manga/" + RANOBE_NAME + ("?fields[]=
                                                                      "]=metadata.close_comments&fields["
                                                                      "]=manga_status_id&fields[]=chap_count&fields["
                                                                      "]=status_id&fields[]=artists&fields[]=format")
-print(f"url к информации о ранобе: {URL_TO_RANOBE}\n")
-print(f"url к главам: {URL_TO_CHAPTERS}\n")
+print(f"url к информации о ранобе: {url_to_ranobe}\n")
+print(f"url к главам: {url_to_chapters}\n")
 
 try:
     shutil.rmtree("cover")
@@ -188,31 +190,31 @@ except:
     pass
 os.makedirs("cover", exist_ok=True)
 os.makedirs("images", exist_ok=True)
-ranobe_info_dict = get_ranobe_info(URL_TO_RANOBE)
+ranobe_info_dict = get_ranobe_info(url_to_ranobe)
 ranobe_title = ranobe_info_dict["title"]
 ranobe_description = ranobe_info_dict["description"]
 ranobe_cover = ranobe_info_dict["cover_url"]
 ranobe_author = ranobe_info_dict["author"]
-ranobe_chapters_dict = get_volume_chapters(url=URL_TO_CHAPTERS, volume=RANOBE_VOLUME)
-
+ranobe_chapters_dict = get_volume_chapters(url=url_to_chapters, volume=ranobe_volume)
 download_cover(ranobe_cover)
 
-book = mkepub.Book(title=ranobe_title, author=ranobe_author, description=ranobe_description)
+book = Book(title=ranobe_title, author=ranobe_author, description=ranobe_description)
 with open('cover/cover.jpg', 'rb') as file:
     book.set_cover(file.read())
 book.set_stylesheet(STYLE)
 
-book.add_page(title=f"{ranobe_title} Том {RANOBE_VOLUME}", content=f"""
-<h1>{ranobe_title} Том  {RANOBE_VOLUME}</h1>
+book.add_page(title=f"{ranobe_title} Том {ranobe_volume}", content=f"""
+<h1>{ranobe_title} Том  {ranobe_volume}</h1>
 <p><i>
 {ranobe_description}
 </i></p>
 """)
 
 for chapter_num, chapter_name in ranobe_chapters_dict.items():
+    print(chapter_num, chapter_name)
     time.sleep(1)
-    url_to_chapter = ("https://api.lib.social/api/manga/" + RANOBE_NAME +
-                      "/chapter?number=" + str(chapter_num) + "&volume=" + str(RANOBE_VOLUME))
+    url_to_chapter = ("https://api.lib.social/api/manga/" + ranobe_name +
+                      "/chapter?number=" + str(chapter_num) + "&volume=" + str(ranobe_volume))
 
     chapter_content, images_dict = get_chapter_content(url=url_to_chapter, chapter_num=chapter_num, chapter_name=chapter_name)
     book.add_page(title=f"Глава {chapter_num}. {chapter_name}", content=chapter_content)
@@ -222,7 +224,7 @@ for chapter_num, chapter_name in ranobe_chapters_dict.items():
                 book.add_image(image[7:], image_file.read())
 
 
-book_name = remove_bad_chars(ranobe_title) + f" Том {RANOBE_VOLUME}.epub"
+book_name = remove_bad_chars(ranobe_title) + f" Том {ranobe_volume}.epub"
 try:
     os.remove(book_name)
 except:
--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import requests
 import time
 import os
 from bs4 import BeautifulSoup
+import collections
+import datetime
+import imghdr
+import itertools
+import jinja2
+import pathlib
+import tempfile
+import uuid
+import zipfile
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -48,7 +58,7 @@ def get_volume_chapters(url:str, volume:str) -> dict:
     for chapter in data:
         if chapter['volume'] == volume:
             chapter_num = chapter['number']
-            chapter_name = chapter['name'] if chapter['name'].strip() != "" else f"Глава {chapters_dict}"
+            chapter_name = chapter['name'] if chapter['name'].strip() != "" else f"Глава {chapter_num}"
             chapters_dict[chapter_num] = chapter_name
     return chapters_dict
 
@@ -154,7 +164,6 @@ def get_chapter_content(url:str, chapter_num:str, chapter_name:str) -> tuple[str
                             if content_of_list_item['type'] == 'paragraph':
                                 content_of_content = content_of_list_item['content'][0]
                                 if content_of_content['type'] == 'text':
-                                    
                                     content += f"<p><li>{content_of_content['text']}</li></p>\n"
                                 else:
                                     raise Exception(element)
@@ -165,3 +174,164 @@ def get_chapter_content(url:str, chapter_num:str, chapter_name:str) -> tuple[str
             else:
                 raise Exception(f"Новый тип {element['type']}, {element}")
     return content, images_dict
+
+
+
+
+###############################################################################
+#  Дальше пофикшенный код mkepub
+###############################################################################
+def mediatype(name):
+    ext = name.split('.')[-1].lower()
+    if ext not in ('png', 'jpg', 'jpeg', 'gif', 'svg','webp'):
+        raise ValueError('Image format "{}" is not supported.'.format(ext))
+    if ext == 'jpg':
+        ext = 'jpeg'
+    return 'image/' + ext
+
+
+def fonttype(name):
+    ext = name.split('.')[-1].lower()
+    mimetypes = {
+        'otf': 'application/font-sfnt',
+        'ttf': 'application/font-sfnt',
+        'woff': 'font/woff',
+        'woff2': 'font/woff2',
+    }
+    if ext not in mimetypes.keys():
+        raise ValueError('Font format "{}" is not supported.'.format(ext))
+    return mimetypes[ext]
+
+
+env = jinja2.Environment(loader=jinja2.PackageLoader('mkepub'))
+env.filters['mediatype'] = mediatype
+env.filters['fonttype'] = fonttype
+
+###############################################################################
+
+Page = collections.namedtuple('Page', 'page_id title children')
+Image = collections.namedtuple('Image', 'image_id name')
+
+
+class Book:
+    """EPUB book."""
+
+    def __init__(self, title, **metadata):
+        """"Create new book."""
+        self.title = title
+        self.metadata = metadata
+
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.root = []
+        self.fonts = []
+        self.images = []
+        self.uuid = uuid.uuid4()
+        self._page_id = map('{:04}'.format, itertools.count(1))
+        self._image_id = map('{:03}'.format, itertools.count(1))
+
+        self.path = pathlib.Path(self.tempdir.name).resolve()
+        for dirname in [
+                'EPUB', 'META-INF', 'EPUB/images', 'EPUB/css', 'EPUB/covers']:
+            (self.path / dirname).mkdir()
+
+        self.set_stylesheet('')
+        self._cover = None
+
+    ###########################################################################
+    # Public Methods
+    ###########################################################################
+
+    def add_page(self, title, content, parent=None):
+        """
+        Add a new page.
+
+        The page will be added as a subpage of the parent. If no parent is
+        provided, the page will be added to the root of the book.
+        """
+        page = Page(next(self._page_id), title, [])
+        self.root.append(page) if not parent else parent.children.append(page)
+        self._write_page(page, content)
+        return page
+
+    def add_image(self, name, data):
+        """Add image file."""
+        self.images.append(Image(next(self._image_id), name))
+        self._add_file(pathlib.Path('images') / name, data)
+
+    def add_font(self, name, data):
+        """Add font file."""
+        self.fonts.append(name)
+        self._add_file(pathlib.Path('fonts') / name, data)
+
+    def set_cover(self, data):
+        """Set the cover image to the given data."""
+        self._cover = 'cover.' + imghdr.what(None, h=data)
+        self._add_file(pathlib.Path('covers') / self._cover, data)
+        self._write('cover.xhtml', 'EPUB/cover.xhtml', cover=self._cover)
+
+    def set_stylesheet(self, data):
+        """Set the stylesheet to the given css data."""
+        self._add_file(
+            pathlib.Path('css') / 'stylesheet.css', data.encode('utf-8'))
+
+    def save(self, filename):
+        """Save book to a file."""
+        if pathlib.Path(filename).exists():
+            raise FileExistsError
+        self._write_spine()
+        self._write('container.xml', 'META-INF/container.xml')
+        self._write_toc()
+        with open(str(self.path / 'mimetype'), 'w') as file:
+            file.write('application/epub+zip')
+        with zipfile.ZipFile(filename, 'w') as archive:
+            archive.write(
+                str(self.path / 'mimetype'), 'mimetype',
+                compress_type=zipfile.ZIP_STORED)
+            for file in self.path.rglob('*.*'):
+                archive.write(
+                    str(file), str(file.relative_to(self.path)),
+                    compress_type=zipfile.ZIP_DEFLATED)
+
+    ###########################################################################
+    # Private Methods
+    ###########################################################################
+
+    def _add_file(self, name, data):
+        """Add a file."""
+        filepath = self.path / 'EPUB' / name
+        if not filepath.parent.exists():
+            filepath.parent.mkdir()
+
+        with open(str(filepath), 'wb') as file:
+            file.write(data)
+
+    def _write(self, template, path, **data):
+        with open(str(self.path / path), 'w', encoding="utf-8") as file:
+            file.write(env.get_template(template).render(**data))
+
+    def _write_page(self, page, content):
+        """Write the contents of the page into an html file."""
+        self._write(
+            'page.xhtml', 'EPUB/page{}.xhtml'.format(page.page_id),
+            title=page.title, body=content)
+
+    def _write_spine(self):
+        self._write(
+            'package.opf', 'EPUB/package.opf',
+            title=self.title,
+            date=datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            pages=list(self._flatten(self.root)), images=self.images,
+            fonts=self.fonts, uuid=self.uuid, cover=self._cover,
+            **self.metadata)
+
+    def _write_toc(self):
+        self._write(
+            'toc.xhtml', 'EPUB/toc.xhtml', pages=self.root, title=self.title)
+        self._write(
+            'toc.ncx', 'EPUB/toc.ncx',
+            pages=self.root, title=self.title, uuid=self.uuid)
+
+    def _flatten(self, tree):
+        for item in tree:
+            yield item
+            yield from self._flatten(item.children)
